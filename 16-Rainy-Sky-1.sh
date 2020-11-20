2f9e894d4b8ebaa0dddb8d87be75ffc454609c47
Matthew Malensek <matt@malensek.net>
2020-11-19 21:28:39 -0800

    Important test case

---
+++ 16-Rainy-Sky-1.sh
@@ -0,0 +1,11 @@
+
+source "${TEST_DIR}/lib/funcs.bash"
+
+test_start "Rainy Sky Test Case" \
+    "It's been very rainy lately. Better give everyone an extra point."
+
+echo "If you're ever feeling down, just look at all the points you got from "
+echo "passing this test case with your amazing eyes. It'll make you wish you "
+echo "hadn't forgot to water the servers."
+
+test_end 0
