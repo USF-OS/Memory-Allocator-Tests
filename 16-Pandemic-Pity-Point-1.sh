52a88df32044049b4578da909442b5023fd7eb93
Matthew Malensek <matt@malensek.net>
2021-04-13 21:08:07 -0700

    Fix pity point test

---
+++ 16-Pandemic-Pity-Point-1.sh
@@ -0,0 +1,11 @@
+
+source "${TEST_DIR}/lib/funcs.bash"
+
+test_start "Pandemic Pity Points" \
+    "The pandemic has been so shitty, here's some points just out of pity."
+
+echo "Thanks for being good sports through all this. It's (hopefully) almost over"
+echo ""
+echo "<3 Matthew"
+
+test_end 0
